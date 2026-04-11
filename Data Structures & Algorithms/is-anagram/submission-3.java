dbaed7026b59b7f49e97e821d8e747f4c37acf81
Sarthak Arora <136910752+variable12354@users.noreply.github.com>
2026-04-11 19:47:01 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] freqArray = new int[26];
+        for(int i = 0;i<s.length();i++){
+            freqArray[s.charAt(i) - 'a']++;
+            freqArray[t.charAt(i) - 'a']--;
+        }
+
+        for(int found:freqArray){
+            if(found > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
